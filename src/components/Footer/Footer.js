929a312098f1a5212ddf5918a2ccf262e1242fd4
Lia <liacicati@gmail.com>
2021-03-13 18:00:41 +0100

    feat : added Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,50 @@
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h3 className="footer__title">
+        Учебный проект Яндекс.Практикум х BeatFilm.
+      </h3>
+      <div className="footer__container">
+        <p className="footer__copyright">© 2021</p>
+
+        <nav className="footer__nav">
+          <ul className="footer__nav-list">
+            <li className="footer__nav-item">
+              <a
+                className="footer__nav-link"
+                href="https://praktikum.yandex.ru/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Яндекс.Практикум
+              </a>
+            </li>
+            <li className="footer__nav-item">
+              <a
+                className="footer__nav-link"
+                href="https://github.com/yandex-praktikum"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Github
+              </a>
+            </li>
+            <li className="footer__nav-item">
+              <a
+                className="footer__nav-link"
+                href="https://www.facebook.com/yandex.praktikum"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Facebook
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
