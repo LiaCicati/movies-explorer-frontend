ae632bab7e00275e539511f7a902e2e6c747bba2
Lia <liacicati@gmail.com>
2021-03-13 17:59:22 +0100

    feat :  added Section Heading component

---
+++ src/components/SectionHeading/SectionHeading.js
@@ -0,0 +1,10 @@
+import "./SectionHeading.css";
+const SectionHeading = ({ title }) => {
+  return (
+    <>
+      <h3 className="title">{title}</h3>
+    </>
+  );
+};
+
+export default SectionHeading;
