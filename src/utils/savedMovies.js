be04b6031c29108ade5cf3e894e0447f90c40ad4
Lia <liacicati@gmail.com>
2021-03-17 16:17:43 +0100

    feat : added movie data

---
+++ src/utils/savedMovies.js
@@ -0,0 +1,36 @@
+const savedMovies = [
+    {
+      id: 1,
+      title: "Дженис: Маленькая девочка грустит",
+      duration: "1ч 17м",
+      image:
+        "http://avatars.mds.yandex.net/get-kinopoisk-image/1629390/180dc51d-73c3-45d6-81e1-6ba289dae892/600x900",
+      saved: true,
+    },
+    {
+      id: 2,
+      title: "Дженис: Маленькая девочка грустит",
+      duration: "1ч 17м",
+      image:
+        "http://avatars.mds.yandex.net/get-kinopoisk-image/1629390/180dc51d-73c3-45d6-81e1-6ba289dae892/600x900",
+      saved: true,
+    },
+    {
+      id: 3,
+      title: "В погоне за Бенкси",
+      duration: "27 минут",
+      image:
+        "https://www.film.ru/sites/default/files/movies/posters/49621981-1239252.jpg",
+      saved: true,
+    },
+    {
+      id: 4,
+      title: "Пи Джей Харви: A dog called money",
+      duration: "1ч 17м",
+      image:
+        "https://glide-assets-media.s3.amazonaws.com/wp-content/uploads/2020/12/02115518/pjharvey35.jpg",
+      saved: true,
+    },
+  ];
+  
+  export default savedMovies;
