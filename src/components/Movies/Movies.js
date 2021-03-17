b0aec14c806d17b0bc50e6bfb9cdf840396b3c11
Lia <liacicati@gmail.com>
2021-03-17 16:36:59 +0100

    feat : added Movies component

---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,21 @@
+import SearchForm from '../SearchForm/SearchForm'
+import MoviesCardList from '../MoviesCardList/MoviesCardList'
+import Footer from '../Footer/Footer'
+import cards from '../../utils/movies';
+
+function Movies() {
+  return (
+    
+    <div className="movies">
+      <SearchForm />
+      <MoviesCardList
+        cards={cards}
+        isSavedMoviesPage={false}
+        buttonMore={true}
+      />
+      <Footer />
+    </div>
+  );
+}
+
+export default Movies;
