6e89af1a7a90c59737e2195f761d26ef27cb19b1
Lia <liacicati@gmail.com>
2021-03-20 16:31:55 +0100

    feat : added validation to Register component

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,15 +3,43 @@ import Greeting from "../Greeting/Greeting";
 import Form from "../Form/Form";
 import Input from "../Input/Input";
 import Auth from "../Auth/Auth";
+import { useState } from "react";
 
 const Register = () => {
   const loggedIn = false;
 
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChangeInput = (evt) => {
+    const { name, validationMessage, value } = evt.target;
+
+    setValues({
+      ...values,
+      [name]: value,
+    });
+
+    setErrors({
+      ...errors,
+      [name]: validationMessage,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <section className="register">
       <Greeting text="Добро пожаловать!" loggedIn={loggedIn} />
-      <Form>
+      <Form name="register" onSubmit={handleSubmit} noValidate>
         <Input
+          auth
           label="Имя"
           id="name"
           name="name"
@@ -19,31 +47,42 @@ const Register = () => {
           minLength="2"
           maxLength="30"
           placeholder="Введите имя"
+          onChange={handleChangeInput}
+          value={values.name}
+          error={errors.name}
         />
         <Input
+          auth
           label="E-mail"
           id="email"
           name="email"
           type="email"
           placeholder="Введите почту"
+          value={values.email}
+          error={errors.email}
+          onChange={handleChangeInput}
         />
 
         <Input
+          auth
           label="Пароль"
           id="password"
           name="password"
           type="password"
           minLength="6"
           placeholder="Введите пароль"
+          value={values.password}
+          error={errors.password}
+          onChange={handleChangeInput}
+        />
+
+        <Auth
+          buttonText="Зарегистрироваться"
+          paragraph="Уже зарегистрированы?"
+          linkText="Войти"
+          href="/signin"
         />
       </Form>
-
-      <Auth
-        buttonText="Зарегистрироваться"
-        paragraph="Уже зарегистрированы?"
-        linkText="Войти"
-        href="/signin"
-      />
     </section>
   );
 };
