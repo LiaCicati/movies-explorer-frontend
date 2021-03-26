4f665eac32356335caabfedb04e371239ac099c2
Lia <liacicati@gmail.com>
2021-03-26 09:38:31 +0100

    feat : profile validation & update data

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,74 +1,81 @@
 import "./Profile.css";
+import Header from "../Header/Header";
 import Greeting from "../Greeting/Greeting";
-import Form from '../Form/Form'
+import Form from "../Form/Form";
 import Input from "../Input/Input";
-import { useState } from 'react'
+import { useContext } from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+import { useFormWithValidation } from "../../utils/utils";
 
-const Profile = () => {
+const Profile = ({ onSignOut, onUpdate }) => {
+  const currentUser = useContext(CurrentUserContext);
+  const { values, errors, isValid, handleChange } = useFormWithValidation();
 
-  const [values, setValues] = useState({
-    name: "",
-    email: "",
-  });
-
-  const [errors, setErrors] = useState({});
-
-  const handleChangeInput = (evt) => {
-    const { name, validationMessage, value } = evt.target;
-
-    setValues({
-      ...values,
-      [name]: value,
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    onUpdate({
+      name: values.name || currentUser.name,
+      email: values.email || currentUser.email,
     });
-
-    setErrors({
-      ...errors,
-      [name]: validationMessage,
-    });
-  };
+  }
 
   return (
-    <section className="profile">
-      <Greeting text="Привет, Лиа!" loggedIn={true} />
-      <Form name="profile" noValidate>
-        <Input
-          editProfile
-          label="Имя"
-          id="name"
-          name="name"
-          type="text"
-          placeholder="Ваше имя"
-          required
-          minLength="2"
-          maxLength="30"
-          autoComplete="off"
-          onChange={handleChangeInput}
-          value={values.name}
-          error={errors.name}
-        />
+    <>
+      <Header loggedIn={true} />
+      <section className="profile">
+        <Greeting text={currentUser.name} loggedIn={true} />
+        <Form name="profile" noValidate onSubmit={handleSubmit}>
+          <Input
+            editProfile
+            label="Имя"
+            id="name"
+            name="name"
+            type="text"
+            placeholder="Ваше имя"
+            required
+            minLength="2"
+            maxLength="30"
+            autoComplete="off"
+            onChange={handleChange}
+            defaultValue={currentUser.name}
+            error={errors.name}
+          />
 
-        <Input
-          editProfile
-          label="Почта"
-          id="email"
-          name="email"
-          type="email"
-          placeholder="Ваша почта"
-          required
-          autoComplete="off"
-          onChange={handleChangeInput}
-          value={values.email}
-          error={errors.email}
-        />
-      </Form>
+          <Input
+            editProfile
+            label="Почта"
+            id="email"
+            name="email"
+            type="email"
+            placeholder="Ваша почта"
+            required
+            autoComplete="off"
+            onChange={handleChange}
+            defaultValue={currentUser.email}
+            error={errors.email}
+          />
 
-      <div className="profile__container">
-        <button className="profile__button">Редактировать</button>
-        <button className="profile__button profile__button_type_logout">
-          Выйти из аккаунта
-        </button>
-      </div>
-    </section>
+          <div className="profile__container">
+            <button
+              type="submit"
+              disabled={!isValid}
+              className={`profile__button ${
+                !isValid ? "profile__button_disabled" : ""
+              }`}
+            >
+              Редактировать
+            </button>
+            <button
+              type="button"
+              className="profile__button profile__button_type_logout"
+              onClick={onSignOut}
+            >
+              Выйти из аккаунта
+            </button>
+          </div>
+        </Form>
+      </section>
+    </>
   );
 };
 
