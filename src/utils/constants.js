2c2ede07099a1d8de9c3f55f99f5a84d262599a9
Lia <liacicati@gmail.com>
2021-03-29 19:36:29 +0200

    feat : added file with constants & validation

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+export const SERVER_ERROR = 'Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз';
+
+export const SUCCESS_PROFILE_UPDATE = 'Данные успешно изменены!';
+
+export const REGISTER_SUCCESS = 'Вы успешно зарегистрировались!';
+
+export const KEYWORD_ENTERED_MESSAGE = 'Нужно ввести ключевое слово';
+
+export const MOVIES_NOT_FOUND = 'Ничего не найдено';
+
+export const SHORT_MOVIE = 40;
